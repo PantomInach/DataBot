80d15d316010469ed3bea1e06395d59887d11e7e
PantomInach <aaron@ralf-saalmueller.de>
2022-04-09 10:29:36 +0200

    Changed version

--- cogs/commandlistener.py
+++ cogs/commandlistener.py
@@ -35,7 +35,7 @@ class Commandlistener(commands.Cog):
 	async def on_ready(self):
 		#Sends message to mods, when bot is online
 		print("Now Online")
-		await self.utils.sendModsMessage(f"Bot is now online.\nVersion:\tDiscordBot in development v1.4.0")
+		await self.utils.sendModsMessage(f"Bot is now online.\nVersion:\tDiscordBot DataBot v1.4.0")
 
 	# When a member joins a guild
 	@commands.Cog.listener()
