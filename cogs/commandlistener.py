c03a89f20a43575665f2cf592ae03a3b3622728f
johnzoki <john.bergmann@outlook.de>
2023-01-08 15:46:02 +0100

    logging state check enhancement

--- cogs/commandlistener.py
+++ cogs/commandlistener.py
@@ -50,8 +50,11 @@ class Commandlistener(commands.Cog):
         await self.utils.sendModsMessage(
             "Bot is now online.\nVersion:\tDiscordBot DataBot v2.3.0"
         )
-        # Sets the bot's presence to "Do not Disturb" to indicate it's not logging.
-        await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Game(str(self.jh.getFromConfig("command_prefix")) + "help"))
+        # Sets the bot's presence to "Online" or "Do not Disturb" to indicate if it's logging or not.
+        if self.jh.getFromConfig("log") == "True":
+            await self.bot.change_presence(status=discord.Status.online, activity=discord.Game(str(self.jh.getFromConfig("command_prefix")) + "help"))
+        else:    
+            await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Game(str(self.jh.getFromConfig("command_prefix")) + "help"))
 
     # When a member joins a guild
     @commands.Cog.listener()
