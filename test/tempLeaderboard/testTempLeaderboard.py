6f4230bf30d62ff8cb5a2ded6cc80f7b1849e720
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-09-16 23:30:09 +0200

    feat: tempLeaderboard backend can now sort data

--- test/tempLeaderboard/testTempLeaderboard.py
+++ test/tempLeaderboard/testTempLeaderboard.py
@@ -4,27 +4,29 @@ import time
 import os
 import shutil
 
-from datahandler.tempLeaderboard import TempLeaderboard, XPTypes
+from datahandler.tempLeaderboard import TempLeaderboard, XPTypes, SortBy
 
 
 class TestUserHandle(unittest.TestCase):
     def setUp(self):
         self.setupTime = time.time()
+        self.validTime = self.setupTime - 86400 * 20
         self.invalidTime = 0
         self.test_data = {
             "1": [
                 (self.invalidTime, XPTypes.VOICE, 1),
                 (self.invalidTime, XPTypes.VOICE, 2),
-                (self.setupTime, XPTypes.VOICE, 3),
-                (self.setupTime, XPTypes.VOICE, 4),
+                (self.validTime, XPTypes.VOICE, 3),
+                (self.validTime, XPTypes.VOICE, 3),
+                (self.setupTime, XPTypes.VOICE, 1),
                 (self.setupTime, XPTypes.TEXT, 5),
                 (self.setupTime, XPTypes.TEXT, 6),
             ],
             "2": [
                 (self.invalidTime, XPTypes.VOICE, 10),
                 (self.invalidTime, XPTypes.VOICE, 11),
-                (self.setupTime, XPTypes.VOICE, 12),
-                (self.setupTime, XPTypes.VOICE, 13),
+                (self.validTime, XPTypes.VOICE, 12),
+                (self.validTime, XPTypes.VOICE, 13),
                 (self.setupTime, XPTypes.TEXT, 14),
                 (self.setupTime, XPTypes.TEXT, 15),
             ]
@@ -35,7 +37,7 @@ class TestUserHandle(unittest.TestCase):
                 db[key] = value
             db.commit()
 
-        self.tempPoll = TempLeaderboard(
+        self.tempLeaderboard = TempLeaderboard(
             "test/data/test.sqlite",
             True,
             30,
@@ -49,24 +51,43 @@ class TestUserHandle(unittest.TestCase):
     def test_removeOutdateUser(self):
         expect = {
             "1": [
-                (self.setupTime, XPTypes.VOICE, 3),
-                (self.setupTime, XPTypes.VOICE, 4),
+                (self.validTime, XPTypes.VOICE, 3),
+                (self.validTime, XPTypes.VOICE, 3),
+                (self.setupTime, XPTypes.VOICE, 1),
                 (self.setupTime, XPTypes.TEXT, 5),
                 (self.setupTime, XPTypes.TEXT, 6),
             ],
             "2": [
-                (self.setupTime, XPTypes.VOICE, 12),
-                (self.setupTime, XPTypes.VOICE, 13),
+                (self.validTime, XPTypes.VOICE, 12),
+                (self.validTime, XPTypes.VOICE, 13),
                 (self.setupTime, XPTypes.TEXT, 14),
                 (self.setupTime, XPTypes.TEXT, 15),
             ]}
-        self.tempPoll.removeAllOutdatedEntries()
+        self.tempLeaderboard.removeAllOutdatedEntries()
         self.assertEqual(tuple(expect.items()),
-                         tuple(self.tempPoll.db.items()))
+                         tuple(self.tempLeaderboard.db.items()))
 
     def test_getAllUserSummedData(self):
         expect = {
-            "1": {XPTypes.OTHER: 0, XPTypes.TEXT: 11, XPTypes.VOICE: 7},
-            "2": {XPTypes.OTHER: 0, XPTypes.TEXT: 29, XPTypes.VOICE: 25},
+            "1": {XPTypes.OTHER: 0, XPTypes.TEXT: 11, XPTypes.VOICE: 7, XPTypes.TEXTCOUNT: 0},
+            "2": {XPTypes.OTHER: 0, XPTypes.TEXT: 29, XPTypes.VOICE: 25, XPTypes.TEXTCOUNT: 0},
         }
-        self.assertEqual(expect, self.tempPoll.getAllUserSummedData())
+        self.assertEqual(expect, self.tempLeaderboard.getAllUserSummedData())
+
+    def test_sortDataWindowBy(self):
+        expect = [
+            ("2", {XPTypes.OTHER: 0, XPTypes.TEXT: 29, XPTypes.VOICE: 25, XPTypes.TEXTCOUNT: 0}),
+            ("1", {XPTypes.OTHER: 0, XPTypes.TEXT: 11, XPTypes.VOICE: 7, XPTypes.TEXTCOUNT: 0}),
+        ]
+        self.assertEqual(expect, self.tempLeaderboard.sortDataWindowBy(SortBy.VOICE_TEXT))
+        expect = [
+            ("2", {XPTypes.OTHER: 0, XPTypes.TEXT: 29, XPTypes.VOICE: 0, XPTypes.TEXTCOUNT: 0}),
+            ("1", {XPTypes.OTHER: 0, XPTypes.TEXT: 11, XPTypes.VOICE: 1, XPTypes.TEXTCOUNT: 0}),
+        ]
+        self.assertEqual(expect, self.tempLeaderboard.sortDataWindowBy(SortBy.VOICE_TEXT, window=10))
+        expect = [
+            ("1", {XPTypes.OTHER: 0, XPTypes.TEXT: 11, XPTypes.VOICE: 1, XPTypes.TEXTCOUNT: 0}),
+            ("2", {XPTypes.OTHER: 0, XPTypes.TEXT: 29, XPTypes.VOICE: 0, XPTypes.TEXTCOUNT: 0}),
+        ]
+        self.assertEqual(expect, self.tempLeaderboard.sortDataWindowBy(SortBy.VOICE, window=10))
+
