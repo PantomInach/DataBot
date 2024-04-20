aaa6262cbf685c986396edf1c4623dc2669a6cb4
Aaron Neugebauer <72529530+PantomInach@users.noreply.github.com>
2024-04-21 00:29:11 +0200

    feat: asynchronous quote fetching (#134)

--- cogs/commandlistener.py
+++ cogs/commandlistener.py
@@ -50,7 +50,7 @@ class Commandlistener(commands.Cog):
         # Sends message to mods, when bot is online
         print("Now Online")
         await self.utils.sendModsMessage(
-            "Bot is now online.\nVersion:\tDiscordBot DataBot v2.7.2"
+            "Bot is now online.\nVersion:\tDiscordBot DataBot v2.7.3"
         )
         # Sets the bot's presence to "Online" or "Do not Disturb" to indicate if it's logging or not.
         if self.ch.getFromConfig("log") == "True":
--- helpfunctions/inspiro.py
+++ helpfunctions/inspiro.py
@@ -1,4 +1,7 @@
 import requests
+import aiohttp
+
+URL = "https://inspirobot.me/api?generate=true"
 
 
 class Inspiro(object):
@@ -10,6 +13,18 @@ class Inspiro(object):
     def getPictureUrl():
         """
         Opens inspirobot site to fetch URL for a random new quote.
+        Blocks thread. Use `getPictureUrlAsync` instead.
         """
-        r = requests.get("https://inspirobot.me/api?generate=true")
-        return str(r.content)[2:-1]
+        r = requests.get(URL)
+        url = str(r.content)[2:-1]
+        r.close()
+        return url
+
+    @staticmethod
+    async def getPictureUrlAsync():
+        """
+        Opens inspirobot site to fetch URL for a random new quote.
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.get(URL) as response:
+                return await response.text()
